112a06fa9a1faf9bd89b73d96104344e0e8f228b
charsilv <262930788+charsilv@users.noreply.github.com>
2026-06-09 20:58:58 +0000

    chore: added check script to run multiple quality gates

---
+++ packages/jaspurr/scripts/check.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+pnpm exec prettier . --check
+
+echo "Checking TS compilation..."
+pnpm exec tsc
+echo "TS compilation ran successfully!"
+
+echo "Running eslint..."
+pnpm exec eslint . --max-warnings 0
+echo "eslint ran successfully!"
