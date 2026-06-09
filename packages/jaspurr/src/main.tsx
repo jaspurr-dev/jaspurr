53ade26a2f91f5b45443fc7632a4845905475b47
Charlie Silver <262930788+charsilv@users.noreply.github.com>
2026-06-09 16:42:08 -0400

    Chore: Ran Prettier Over the Main Package (#9)

--- packages/jaspurr/src/main.tsx
+++ packages/jaspurr/src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@styles/index.css'
-import App from '@routes/App'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import '@styles/index.css';
+import App from '@routes/App';
 
-const root:HTMLElement | null = document.getElementById('root');
+const root: HTMLElement | null = document.getElementById('root');
 if (!root) throw new Error('Root element not found in the DOM');
 createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
