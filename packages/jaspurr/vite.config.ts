b4a060f877b18626d185546222db2e50a3031b5a
charsilv <262930788+charsilv@users.noreply.github.com>
2026-06-09 20:39:06 +0000

    chore: ran prettier over the main package

--- packages/jaspurr/vite.config.ts
+++ packages/jaspurr/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-    }
-  },
-  server: {
-    host: true
-  }
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@routes': path.resolve(__dirname, './src/routes'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+        },
+    },
+    server: {
+        host: true,
+    },
+});
