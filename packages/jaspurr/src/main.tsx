6cfad65717c38b67fa9214113c5141f1e0396f3a
charsilv <262930788+charsilv@users.noreply.github.com>
2026-06-09 16:19:30 +0000

    chore: configured strict eslint, fixed found errors

--- packages/jaspurr/src/main.tsx
+++ packages/jaspurr/src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import '@styles/index.css'
 import App from '@routes/App'
 
-createRoot(document.getElementById('root')!).render(
+const root:HTMLElement | null = document.getElementById('root');
+if (!root) throw new Error('Root element not found in the DOM');
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
