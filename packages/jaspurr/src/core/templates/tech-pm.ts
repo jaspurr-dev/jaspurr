24910db9b5b87ea07e628e997f1773baf49814e2
Charlie Silver <262930788+charsilv@users.noreply.github.com>
2026-06-19 16:08:34 -0400

    [CF-Pages-Skip] Feat: Core Data Types (#20)

---
+++ packages/jaspurr/src/core/templates/tech-pm.ts
@@ -0,0 +1,13 @@
+import {Output, Persona, type Template} from '@/core/template';
+
+export const techPm = {
+    id: 'tech-pm-type-a',
+    name: 'Type A - Tech PM',
+    persona: Persona.TechPm,
+    steps: [
+        'In one question: (1) Ask what needs to be shipped and when. (2) Ask what tech constraints are. \
+        (3) Ask what ideal happy path UX is. (4) Ask what failure UX looks like.  \
+        (5) Ask what the current state of the project is.',
+    ],
+    output: Output.PrioritizedList,
+} satisfies Template;
