141a8ab2b281b52bb5c84b57779c2aa458815a28
charsilv <262930788+charsilv@users.noreply.github.com>
2026-06-22 20:54:57 +0000

    feat: added serialize data types + allowlist tests

--- packages/jaspurr/src/core/sanitize.ts
+++ packages/jaspurr/src/core/sanitize.ts
@@ -1,8 +1,10 @@
+export const NOT_ALLOWED = /[^\t\n\x20-\x7E]/g;
+
 export function sanitize(text: string): string {
     return text
         .normalize('NFC') // NFC normalization.
         .replace(/\r\n?/g, '\n') // CRLF normalization.
-        .replace(/[^\t\n\x20-\x7E]/g, '') // ASCII allowlist.
+        .replace(new RegExp(NOT_ALLOWED.source, 'gu'), '') // ASCII allowlist.
         .replace(/[ \t]+$/gm, '') // Strip trailing space per line.
         .trim(); // Strip leading/trailing white space.
 }
