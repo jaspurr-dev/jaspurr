5a56c01087567968be225f3a17cd7f6013a16868
charsilv <262930788+charsilv@users.noreply.github.com>
2026-06-23 19:32:49 +0000

    feat: added a couple more reducer test cases

--- packages/jaspurr/src/tests/core/reducer.test.ts
+++ packages/jaspurr/src/tests/core/reducer.test.ts
@@ -2,6 +2,7 @@ import {describe, it, expect} from 'vitest';
 import {compositionReducer} from '@/core/reducer';
 import {defaultComposition, TEMPLATES} from '@/core/templates';
 import {fromTemplate, Output} from '@/core/template';
+import type {CompositionAction} from '@/core/actions';
 
 describe('composition reducer', () => {
     it('default composition is tech pm', () => {
@@ -11,6 +12,35 @@ describe('composition reducer', () => {
         const c = fromTemplate(base);
         expect(defaultComposition).toStrictEqual(c);
     });
+
+    it('throws on an unsupported action in the reducer', () => {
+        expect(() =>
+            compositionReducer(defaultComposition, {
+                type: 'foo',
+            } as unknown as CompositionAction)
+        ).toThrow();
+    });
+
+    it('deleteStep removes the step at index', () => {
+        const updated = compositionReducer(defaultComposition, {
+            type: 'deleteStep',
+            index: 0,
+        });
+        expect(updated.steps).toHaveLength(defaultComposition.steps.length - 1);
+
+        const updateMore = compositionReducer(updated, {
+            type: 'deleteStep',
+            index: 2,
+        });
+        expect(updateMore.steps).toHaveLength(
+            defaultComposition.steps.length - 2
+        );
+
+        expect(updateMore.steps).not.toContain(defaultComposition.steps[0]);
+        expect(updateMore.steps).not.toContain(defaultComposition.steps[3]);
+        expect(updateMore.steps).not.toStrictEqual(defaultComposition.steps);
+    });
+
     it('setOutput changes the output format of the composition', () => {
         const updated = compositionReducer(defaultComposition, {
             type: 'setOutput',
