63c9458ec5a5adecc0ff339ce6ff6043cbdab8bc
charsilv <262930788+charsilv@users.noreply.github.com>
2026-06-23 21:23:37 +0000

    feat: removed vite template content for clean project

--- packages/jaspurr/src/main.tsx
+++ packages/jaspurr/src/main.tsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import '@styles/index.css';
-import App from '@routes/App';
+import {App} from '@routes/App';
 
 const root: HTMLElement | null = document.getElementById('root');
 if (!root) throw new Error('Root element not found in the DOM');
