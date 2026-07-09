a1438a55f7cf45d2b04b5ee7f0a808202201e774
charsilv <262930788+charsilv@users.noreply.github.com>
2026-07-09 19:52:55 +0000

    feat: added card component to the home route

--- packages/jaspurr/src/routes/Home.tsx
+++ packages/jaspurr/src/routes/Home.tsx
@@ -1,7 +1,21 @@
+import {Text, TextTitle} from '@/primitives';
+import {Card} from '@components/card/Card';
+import style from './Home.module.css';
+
 export function RouteHome() {
+    const templates = ['Template1', 'Template2', 'Template3', 'Template4'];
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={style.home}>
+            {/* TODO: Placeholder header space */}
+            <header className={style.header}>
+                <TextTitle>Header</TextTitle>
+            </header>
+            {templates.map((t) => (
+                <Card>
+                    <TextTitle>{t}</TextTitle>
+                    <Text>Placeholder template content.</Text>
+                </Card>
+            ))}
         </div>
     );
 }
